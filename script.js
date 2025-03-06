2550d9afd33d5047c95a7d0fad8f7180a830595b
universoonline <universoonline61@gmail.com>
2025-03-06 18:39:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://runpfgacmjemflyecubp.supabase.co"; // Substitua pela sua URL
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnBmZ2FjbWplbWZseWVjdWJwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MDUyODczOCwiZXhwIjoyMDU2MTA0NzM4fQ.3MD4q-DssyXdBUOw1IcrU9FAQZCdpiEElCz55kfz0js"; // Substitua pela sua chave
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnBmZ2FjbWplbWZseWVjdWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1Mjg3MzgsImV4cCI6MjA1NjEwNDczOH0.sbmYJqCzMOd9A9nUFuw-Og8e156YGEwcME5dLeV6aLY"; // Substitua pela sua chave
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
