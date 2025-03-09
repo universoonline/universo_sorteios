3757935f49ca829057e9b184474891a2a7a897f3
universoonline <universoonline61@gmail.com>
2025-03-09 18:42:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const SUPABASE_URL = "https://runpfgacmjemflyecubp.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnBmZ2FjbWplbWZseWVjdWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1Mjg3MzgsImV4cCI6MjA1NjEwNDczOH0.sbmYJqCzMOd9A9nUFuw-Og8e156YGEwcME5dLeV6aLY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1bnBmZ2FjbWplbWZseWVjdWJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1Mjg3MzgsImV4cCI6MjA1NjEwNDczOH0.sbmYJqCzMOd9A9nUFuw-Og8e156YGEwcME5dLeV6aLY"; // sua chave anônima correta
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log("Supabase carregado:", supabase);
+
 async function cadastrarUsuario() {
     try {
         // Pegando os valores dos inputs
@@ -20,7 +21,7 @@ async function cadastrarUsuario() {
 
         // Enviando os dados para o Supabase
         const { data, error } = await supabase
-            .from('universo_sorteio') // Nome da tabela no Supabase
+            .from("universo_sorteio") // Nome da tabela no Supabase
             .insert([{ nome, email, whatsapp, data_cadastro }]);
 
         if (error) {
@@ -35,7 +36,7 @@ async function cadastrarUsuario() {
         alert("Ocorreu um erro. Tente novamente.");
     }
 }
+
 document.addEventListener("DOMContentLoaded", function () {
     window.cadastrarUsuario = cadastrarUsuario;
 });
-
